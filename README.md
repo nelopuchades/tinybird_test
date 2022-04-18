cabf81b9f1bef83eb528e5db07e963b01c9b1b1c
Nelo Puchades <correo@nelo.dev>
2022-04-18 15:49:54 +0200

    last style fixes + added missing handlers in onBodyLoad function

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
 # Tinybird Frontend Test
 
-This repository contains the resolution of a test from Tinybird. To start, please check the `Before start` section.
+![Tinybird logo](assets/tinybird_logo.png)
 
-## Before start
+This repository contains the resolution for a test from Tinybird. To start, please check the `Instructions` section.
 
-Run the following command to install the dependencies:
+## 📝 Instructions
+
+Run the following command to install the dependencies 👇
 
 ```
 npm install
 ```
 
 And open `index.html`
+
+## 🤔 Why not tests?
+
+I've decided not to add any tests because I've not been able to find a way to test the application with html and js without any framework.
+I thought of dividing every functionality into exportable functions in order to be able to do unit tests, but CORS, modules, etc gave me many problems.
+In order to don't mess up the test, I decided then to not add any test.
+
+## 🔜 Follow ups
+
+- Migrate the application to any JS framework (Vue, React) in order to provide a much easier testing process
+- Add a way of creating your own filters dinamically without having to modify sql query in index.js
--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-'use strict';
+"use strict";
 
-let vendor = '';
-let payType = '';
+let vendor = "";
+let payType = "";
 let withTip = null;
-let fromDate = '2017-01-01';
-let toDate = '2017-12-31';
+let fromDate = "2017-01-01";
+let toDate = "2017-12-31";
 
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
 });
 
-const fromDateInput = document.getElementById('time-from');
-const toDateInput = document.getElementById('time-to');
-const tipCheckbox = document.getElementById('tip');
+const fromDateInput = document.getElementById("time-from");
+const toDateInput = document.getElementById("time-to");
+const tipCheckbox = document.getElementById("tip");
 
 const getData = async () => {
-  const url = new URL('https://api.tinybird.co/v0/pipes/yellow_tripdata_2017_pipe.json');
+  const url = new URL(
+    "https://api.tinybird.co/v0/pipes/yellow_tripdata_2017_pipe.json"
+  );
   url.searchParams.append(
-    'q',
+    "q",
     `
     select sum(total_amount) as total_amount,
     count(*) as total_trips,
@@ -26,15 +28,15 @@ const getData = async () => {
     avg(trip_distance) as avg_trip_distance,
     avg(datediff(minute, tpep_pickup_datetime, tpep_dropoff_datetime)) as avg_trip_time
     from _ where tpep_pickup_datetime between '${fromDate} 00:00:00' and '${toDate} 23:59:59'
-    ${vendor !== '' ? `and vendorid=${vendor}` : ''}
-    ${payType !== '' ? `and payment_type=${payType}` : ''}
-    ${withTip ? 'and tip_amount>0' : ''}
+    ${vendor !== "" ? `and vendorid=${vendor}` : ""}
+    ${payType !== "" ? `and payment_type=${payType}` : ""}
+    ${withTip ? "and tip_amount>0" : ""}
     `
   );
   const result = await fetch(url, {
     headers: new Headers({
       Authorization:
-        'Bearer p.eyJ1IjogIjdmOTIwMmMzLWM1ZjctNDU4Ni1hZDUxLTdmYzUzNTRlMTk5YSIsICJpZCI6ICJmZTRkNWFiZS05ZWIyLTRjMjYtYWZiZi0yYTdlMWJlNDQzOWEifQ.P67MfoqTixyasaMGH5RIjCrGc0bUKvBoKMwYjfqQN8c',
+        "Bearer p.eyJ1IjogIjdmOTIwMmMzLWM1ZjctNDU4Ni1hZDUxLTdmYzUzNTRlMTk5YSIsICJpZCI6ICJmZTRkNWFiZS05ZWIyLTRjMjYtYWZiZi0yYTdlMWJlNDQzOWEifQ.P67MfoqTixyasaMGH5RIjCrGc0bUKvBoKMwYjfqQN8c",
     }),
   })
     .then((r) => r.json())
@@ -43,14 +45,24 @@ const getData = async () => {
 
   if (result && result.data) {
     const data = result.data[0];
-    document.getElementById('total_trips').innerHTML = `${data.total_trips.toLocaleString()} trips`;
-    document.getElementById('total_amount').innerHTML = formatter.format(data.total_amount);
-    document.getElementById('cost_per_trip').innerHTML = formatter.format(
+    document.getElementById(
+      "total_trips"
+    ).innerHTML = `${data.total_trips.toLocaleString()} trips`;
+    document.getElementById("total_amount").innerHTML = formatter.format(
+      data.total_amount
+    );
+    document.getElementById("cost_per_trip").innerHTML = formatter.format(
       data.total_trips ? data.total_amount / data.total_trips : 0
     );
-    document.getElementById('distance').innerHTML = `${data.avg_trip_distance.toFixed(2)} miles`;
-    document.getElementById('cost_per_mile').innerHTML = formatter.format(data.total_distance / data.total_amount);
-    document.getElementById('duration').innerHTML = `${data.avg_trip_time.toFixed(2)} mins`;
+    document.getElementById(
+      "distance"
+    ).innerHTML = `${data.avg_trip_distance.toFixed(2)} miles`;
+    document.getElementById("cost_per_mile").innerHTML = formatter.format(
+      data.total_distance / data.total_amount
+    );
+    document.getElementById(
+      "duration"
+    ).innerHTML = `${data.avg_trip_time.toFixed(2)} mins`;
   }
 };
 
@@ -58,81 +70,99 @@ const resetParams = (paramToDelete) => {
   const searchParams = new URLSearchParams(window.location.search);
   if (paramToDelete) searchParams.delete(paramToDelete);
   const newURL = `${window.location.pathname}?${searchParams.toString()}`;
-  history.pushState(null, '', newURL);
+  history.pushState(null, "", newURL);
 };
 
 const setQueryParam = (queryParam, queryValue) => {
   const searchParams = new URLSearchParams(window.location.search);
   searchParams.set(queryParam, queryValue);
   const newURL = `${window.location.pathname}?${searchParams.toString()}`;
-  history.pushState(null, '', newURL);
+  history.pushState(null, "", newURL);
 };
 
 const onBodyLoad = () => {
   const searchParams = new URLSearchParams(window.location.search);
-  const selectedVendor = searchParams.get('driver');
+  const selectedVendor = searchParams.get("driver");
   if (selectedVendor) {
     vendor = selectedVendor;
     switch (selectedVendor) {
-      case '1':
-      case '2':
+      case "1":
+      case "2":
         document.getElementById(`vendor-${selectedVendor}`).checked = true;
         break;
       default:
-        document.getElementById('vendor-all').checked = true;
+        document.getElementById("vendor-all").checked = true;
     }
   } else {
-    vendor = '';
+    vendor = "";
   }
 
-  const payTypeSelected = searchParams.get('pay_type');
-  if (payTypeSelected) {
-    payType = payTypeSelected;
-    switch (payTypeSelected) {
-      case '1':
-      case '2':
-        document.getElementById(`pay-type-${payTypeSelected}`).checked = true;
-        break;
-      default:
-        document.getElementById('pay-type-all').checked = true;
-    }
-  } else {
-    payType = '';
-  }
-
-  const fromDateParam = searchParams.get('from');
+  const fromDateParam = searchParams.get("from");
   if (fromDateParam) {
     fromDate = fromDateParam;
     fromDateInput.value = fromDateParam;
   } else {
-    fromDate = '2017-01-01';
+    fromDate = "2017-01-01";
     fromDateInput.value = fromDate;
+  }
+
+  const toDateParam = searchParams.get("to");
+  if (toDateParam) {
+    toDate = toDateParam;
+    toDateInput.value = toDateParam;
+  } else {
+    toDate = "2017-12-31";
+    toDateInput.value = toDate;
+  }
+
+  const payTypeSelected = searchParams.get("pay_type");
+  if (payTypeSelected) {
+    payType = payTypeSelected;
+    switch (payTypeSelected) {
+      case "1":
+      case "2":
+        document.getElementById(`pay-type-${payTypeSelected}`).checked = true;
+        break;
+      default:
+        document.getElementById("pay-type-all").checked = true;
+    }
+  } else {
+    payType = "";
+  }
+
+  const withTipParam = searchParams.get("with_tip");
+  if (withTipParam) {
+    withTip = withTipParam;
+    tipCheckbox.checked = withTipParam;
+  } else {
+    withTip = null;
+    tipCheckbox.checked = false;
   }
 
   getData();
 };
 
-const vendorClickHandler = (e) => {
+const onVendorChangeHandler = (e) => {
   const vendorSelected = e.target.value;
   vendor = vendorSelected;
 
   if (vendorSelected) {
-    setQueryParam('driver', vendorSelected);
+    setQueryParam("driver", vendorSelected);
   } else {
-    resetParams('driver');
+    resetParams("driver");
   }
 
   getData();
 };
 
-const payTypeClickHandler = (e) => {
+const onPayTypeChangeHandler = (e) => {
   const payTypeSelected = e.target.value;
   payType = payTypeSelected;
 
   if (payTypeSelected) {
-    setQueryParam('pay_type', payTypeSelected);
+    setQueryParam("pay_type", payTypeSelected);
   } else {
-    resetParams('pay_type');
+    resetParams("pay_type");
   }
 
   getData();
@@ -140,7 +170,7 @@ const payTypeClickHandler = (e) => {
 
 const onDateChangeHandler = (e, dateDirection) => {
   const date = e.target.value;
-  if (dateDirection === 'from') {
+  if (dateDirection === "from") {
     fromDate = date;
     toDateInput.min = date;
   } else {
@@ -157,28 +187,28 @@ const onDateChangeHandler = (e, dateDirection) => {
   getData();
 };
 
-const tipClickHandler = () => {
+const onTipChangeHandler = () => {
   const isChecked = tipCheckbox.checked;
 
   if (isChecked) {
     withTip = isChecked;
-    setQueryParam('with_tip', isChecked);
+    setQueryParam("with_tip", isChecked);
   } else {
     withTip = null;
-    resetParams('with_tip');
+    resetParams("with_tip");
   }
 
   getData();
 };
 
-fromDateInput.addEventListener('change', (e) => onDateChangeHandler(e, 'from'));
-toDateInput.addEventListener('change', (e) => onDateChangeHandler(e, 'to'));
-tipCheckbox.addEventListener('input', tipClickHandler);
+fromDateInput.addEventListener("change", (e) => onDateChangeHandler(e, "from"));
+toDateInput.addEventListener("change", (e) => onDateChangeHandler(e, "to"));
+tipCheckbox.addEventListener("input", onTipChangeHandler);
 
-document.getElementsByName('vendor-group').forEach((radioInput) => {
-  radioInput.addEventListener('click', vendorClickHandler);
+document.getElementsByName("vendor-group").forEach((radioInput) => {
+  radioInput.addEventListener("click", onVendorChangeHandler);
 });
 
-document.getElementsByName('pay-type-group').forEach((radioInput) => {
-  radioInput.addEventListener('click', payTypeClickHandler);
+document.getElementsByName("pay-type-group").forEach((radioInput) => {
+  radioInput.addEventListener("click", onPayTypeChangeHandler);
 });
